af8a9bc77f7830f6e06ace4a597fa42aea1d3d6a
赵思远 <zhaosiyuan@corp-ci.com>
2019-12-30 14:48:00 +0800

    optimize: 不使用 % 运算，性能 opt write 性能提升 11 倍，read 提升 6倍，苍天呐，% 运算居然耗费这么多计算资源

--- circular-buffer/circular_buffer.go
+++ circular-buffer/circular_buffer.go
@@ -16,7 +16,10 @@ func (buff *Buffer) ReadByte() (byte, error) {
 		return 0, errors.New("no available data")
 	}
 
-	pos := (buff.next + buff.available) % buff.size
+	pos := buff.next + buff.available
+	if pos >= buff.size {
+		pos -= buff.size 
+	}
 	buff.available += 1
 	return buff.bytes[pos], nil
 }
@@ -27,14 +30,20 @@ func (buff *Buffer) WriteByte(b byte) error {
 	}
 
 	buff.bytes[buff.next] = b
-	buff.next = (buff.next + 1) % buff.size
+	buff.next = buff.next + 1
+	if buff.next >= buff.size {
+		buff.next -= buff.size
+	}
 	buff.available -= 1
 	return nil
 }
 
 func (buff *Buffer) Overwrite(b byte) {
 	buff.bytes[buff.next] = b
-	buff.next = (buff.next + 1) % buff.size
+	buff.next = buff.next + 1
+	if buff.next >= buff.size {
+		buff.next -= buff.size
+	}
 	if buff.available > 0 {
 		buff.available -= 1
 	}
@@ -53,3 +62,19 @@ func (buff *Buffer) Reset() {
 	buff.next = 0
 	buff.available = buff.size
 }
+/**
+Version 1: 
+pos := buff.next + buff.available
+if pos >= buff.size {
+	pos -= buff.size 
+}
+BenchmarkOverwrite-8    1000000000               1.93 ns/op     519052853364.44 MB/s           0 B/op          0 allocs/op
+BenchmarkWriteRead-8    500000000                3.33 ns/op     150156769132.68 MB/s           0 B/op          0 allocs/op
+
+Version 2:
+pos := (buff.next + buff.available) % buff.size
+BenchmarkOverwrite-8    100000000               13.7 ns/op      7289753300.06 MB/s             0 B/op          0 allocs/op
+BenchmarkWriteRead-8    100000000               18.9 ns/op      5304322677.14 MB/s             0 B/op          0 allocs/op
+
+so, mod will spend more time
+**/
